79c371bb7a95219c008493dc802aa55d66df7432
Reid Miller <reid.miller@wisc.edu>
2020-07-29 09:07:00 -0500

    Updates

---
+++ precache-manifest.e1267d22c1b21f646d5f73db517f359b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e79b3c5bf1fc69f176992205cec72a",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a76e965a295db1c09de4",
+    "url": "/tic-tac-toe/static/css/2.c548bf2f.chunk.css"
+  },
+  {
+    "revision": "7539db952305bce62030",
+    "url": "/tic-tac-toe/static/css/main.3704a938.chunk.css"
+  },
+  {
+    "revision": "a76e965a295db1c09de4",
+    "url": "/tic-tac-toe/static/js/2.e2a97095.chunk.js"
+  },
+  {
+    "revision": "7539db952305bce62030",
+    "url": "/tic-tac-toe/static/js/main.c28df176.chunk.js"
+  },
+  {
+    "revision": "027d780fd6ac3f298ef0",
+    "url": "/tic-tac-toe/static/js/runtime-main.ecf72bd5.js"
+  }
+]);
